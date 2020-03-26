f5c6eb8e9a05ac75f18bda303d70562f5471004d
Shayne Zhang (NSB - CN/Qingdo) <Shayne.Zhang@nokia-sbell.com>
2020-03-26 08:29:09 +0000

    add quickly switch buffer

--- layer/shayne_misc/config.el
+++ layer/shayne_misc/config.el
@@ -11,3 +11,9 @@
 
 (global-set-key (kbd "C-c w") 'ska-point-to-register)
 (global-set-key (kbd "C-c j") 'ska-jump-to-register)
+
+;;快速切换至上个buffer
+(global-set-key (kbd "<backtab>") #'(lambda ()
+                                      (interactive)
+                                      (switch-to-buffer (other-buffer (current-buffer) 1))))
+
